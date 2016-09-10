057f9218d71b0d1a0bc31eb1b78a5c28d277a453
kevinthomas <kthomas1208@gmail.com>
2016-09-09 23:26:17 -0400

    Added intent from main to detail activity

--- app/src/main/java/com/dreammist/foodwheel/MainActivity.java
+++ app/src/main/java/com/dreammist/foodwheel/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dreammist.foodwheel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView)findViewById(R.id.restaurantImage);
+        image.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View view) {
+                                         Intent intent = new Intent(view.getContext(), DetailActivity.class);
+                                         startActivity(intent);
+                                     }
+                                 }
+        );
     }
+
 }
