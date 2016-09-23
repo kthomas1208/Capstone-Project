638ffce63db5899595c19e6348685a28c4fb1ae3
kevinthomas <kthomas1208@gmail.com>
2016-09-23 00:55:42 -0400

    Removed comment

--- app/src/main/java/com/dreammist/foodwheel/MainActivity.java
+++ app/src/main/java/com/dreammist/foodwheel/MainActivity.java
@@ -304,9 +304,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             RestaurantCursor restaurant = where.query(getContentResolver());
             restaurant.moveToNext();
             ((TextView)mRestaurantTitle).setText(restaurant.getName());
-            // https://maps.googleapis.com/maps/api/place/photo?maxwidth=400
-            // &photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU
-            // &key=AIzaSyAeDHr2K0fPSNtzcJhY8zveVxR7U3J94UY
 
             final String PHOTOS_BASE_URL =
                     "https://maps.googleapis.com/maps/api/place/photo?";
