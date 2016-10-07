09d93388500168d5d6fd8f1b08ef4a0169d811bf
kevinthomas <kthomas1208@gmail.com>
2016-10-07 00:17:12 -0400

    Added image to widget

--- app/src/main/java/com/dreammist/foodwheel/RestaurantWidget.java
+++ app/src/main/java/com/dreammist/foodwheel/RestaurantWidget.java
@@ -13,10 +13,12 @@ public class RestaurantWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
+        String widgetText = "McDowell's";
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.restaurant_widget);
-        //views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.widget_restaurant_name, widgetText);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -39,5 +41,6 @@ public class RestaurantWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
 }
 
