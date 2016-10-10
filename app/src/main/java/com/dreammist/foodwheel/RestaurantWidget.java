cb3b83a984fdd88207381de6dcafc915beac2544
kevinthomas <kthomas1208@gmail.com>
2016-10-10 00:13:09 -0400

    Launching DetailActivity properly from widget

--- app/src/main/java/com/dreammist/foodwheel/RestaurantWidget.java
+++ app/src/main/java/com/dreammist/foodwheel/RestaurantWidget.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
@@ -20,14 +21,9 @@ import java.util.Random;
 public class RestaurantWidget extends AppWidgetProvider {
 
     private static final String FIND_ACTION = "findRestaurantClicked";
-    private static final String CLICK_RESTAURANT_ACTION = "restaurantNameClicked";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-//        // There may be multiple widgets active, so update all of them
-//        for (int appWidgetId : appWidgetIds) {
-//            updateAppWidget(context, appWidgetManager, appWidgetId);
-//        }
         RemoteViews remoteViews;
         ComponentName restaurantWidget;
 
@@ -36,19 +32,6 @@ public class RestaurantWidget extends AppWidgetProvider {
 
         // Set action for find button
         remoteViews.setOnClickPendingIntent(R.id.widget_button, getPendingSelfIntent(context,FIND_ACTION));
-
-        // Set intent for pressing restaurant name
-        //remoteViews.setOnClickPendingIntent(R.id.widget_restaurant_name, getPendingSelfIntent(context,CLICK_RESTAURANT_ACTION));
-        Intent detailIntent = new Intent(context, MainActivity.class);
-        // TODO: 10/9/16 Figure out how to get the CORRECT placeId data from the DB
-//        RestaurantSelection where = new RestaurantSelection();
-//        RestaurantCursor restaurant = where.query(context.getContentResolver());
-//        restaurant.moveToNext();
-//        detailIntent.putExtra(MainActivity.PLACE_ID, restaurant.getPlaceId());
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,detailIntent,0);
-        remoteViews.setOnClickPendingIntent(R.id.widget_restaurant_name, pendingIntent);
-
 
         appWidgetManager.updateAppWidget(restaurantWidget,remoteViews);
     }
@@ -78,39 +61,55 @@ public class RestaurantWidget extends AppWidgetProvider {
                 // Set the name of the restaurant for display
                 remoteViews.setTextViewText(R.id.widget_restaurant_name, restaurant.getName());
 
+                // Launch a Detail Activity when you click the restaurant name
+                Intent detailIntent = new Intent(context, DetailActivity.class);
+                detailIntent.putExtra(MainActivity.PLACE_ID, restaurant.getPlaceId());
+                detailIntent.putExtra(MainActivity.PHOTO_URI,
+                        createPhotoURI(restaurant.getPhotoRef()));
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, detailIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+                remoteViews.setOnClickPendingIntent(R.id.widget_restaurant_name, pendingIntent);
+
                 appWidgetManager.updateAppWidget(restaurantWidget, remoteViews);
             }
             else {
-                Toast.makeText(context, context.getString(R.string.widget_warning),
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, context.getString(R.string.widget_warning), Toast.LENGTH_SHORT).show();
             }
             restaurant.close();
         }
-        if (CLICK_RESTAURANT_ACTION.equals(intent.getAction())) {
-//            // Launch the Detail Activity with the restaurant data
-//            Intent detailIntent = new Intent(context, DetailActivity.class);
-//            RestaurantSelection where = new RestaurantSelection();
-//            RestaurantCursor restaurant = where.query(context.getContentResolver());
-//            if(restaurant.moveToNext()) {
-//
-//                detailIntent.putExtra(MainActivity.PLACE_ID, restaurant.getPlaceId());
-//                if (!restaurant.getPhotoRef().isEmpty())
-//                    detailIntent.putExtra(MainActivity.PHOTO_URI, restaurant.getPhotoRef());
-//
-//                context.startActivity(detailIntent);
-//            }
-//            else {
-//                Toast.makeText(context, context.getString(R.string.widget_warning),
-//                        Toast.LENGTH_SHORT).show();
-//            }
-        }
-
     }
 
     protected PendingIntent getPendingSelfIntent(Context context, String action) {
         Intent intent = new Intent(context, getClass());
         intent.setAction(action);
         return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+    /**
+     * Creates a Photo URI for a image using a photo reference from the Places API
+     * @param photoRef photo reference string from Places API for a specific location
+     * @return a fully qualified URI that will return an image for a specific location
+     */
+    public String createPhotoURI(String photoRef) {
+        // Get a photo of the restaurant location
+        final String PHOTOS_BASE_URL =
+                "https://maps.googleapis.com/maps/api/place/photo?";
+        final String MAXWIDTH_PARAM = "maxwidth";
+        final String PHOTOREF_PARAM = "photoreference";
+        final String KEY_PARAM = "key";
+
+        String API_KEY = BuildConfig.MyApiKey;
+
+        if (!photoRef.equals("null")) {
+            Uri builtUri = Uri.parse(PHOTOS_BASE_URL).buildUpon()
+                    .appendQueryParameter(MAXWIDTH_PARAM, "500")
+                    .appendQueryParameter(PHOTOREF_PARAM, photoRef)
+                    .appendQueryParameter(KEY_PARAM, API_KEY)
+                    .build();
+
+            return builtUri.toString();
+        }
+        return "";
     }
 
     @Override
