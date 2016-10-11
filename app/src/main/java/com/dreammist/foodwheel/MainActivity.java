95d6b8b63900cd18b96ba6500708823aade92445
kevinthomas <kthomas1208@gmail.com>
2016-10-10 23:05:40 -0400

    Added Toolbar

--- app/src/main/java/com/dreammist/foodwheel/MainActivity.java
+++ app/src/main/java/com/dreammist/foodwheel/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
@@ -64,9 +65,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Always delete data from db when launching app
-        //getContentResolver().delete(RestaurantColumns.CONTENT_URI,null,null);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
